bc412fab6a874987d14770249a01efe2c8e589da
Michał Wiaderkowicz <michal.wiaderkowicz@onet.pl>
2023-10-25 20:48:28 +0200

    Add app

--- dist/js/script.js
+++ dist/js/script.js
@@ -51,7 +51,32 @@ const select = {
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
-  
+
+  class Product{
+    constructor(){
+      const thisProduct = this;
+
+      console.log('new Product:', thisProduct);
+    }
+  }
+  const app = {
+    initMenu: function(){
+      const testProduct = new Product();
+      console.log('testProduct:', testProduct);
+      },
+
+      init: function(){
+        const thisApp = this;
+        console.log('*** App String ***');
+        console.log('thisApp:', thisApp);
+        console.log('classNames:', classNames);
+        console.log('settings:', settings);
+        console.log('templates:', templates);
+
+        thisApp.initMenu();
+      },
+    };
+    
   app.init();
 
 }
